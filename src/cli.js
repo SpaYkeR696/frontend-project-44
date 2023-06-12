cf5f3d222fff061620abd7965f9392383f1e169d
SpaYkeR696 <spayker666999@gmail.com>
2023-06-12 11:01:54 +0300

    fix linter bugs

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
+
 const name = () => {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  // eslint-disable-next-line no-console
+  console.log(`Hello, ${userName}!`);
 };
 
 export default name;
