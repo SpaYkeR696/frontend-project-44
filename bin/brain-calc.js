5563fa996706916cfb0e99ae99bce6ce15049c7a
SpaYkeR696 <spayker666999@gmail.com>
2023-06-14 09:41:08 +0300

    creating a calculator game

---
+++ bin/brain-calc.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import game from '../src/games/game-calc.js';
+import rules from '../src/index.js';
+import { name } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = name();
+console.log(`Hello, ${userName}`);
+
+const gameRules = rules();
+gameRules.setUserName(userName);
+gameRules.reset();
+game(gameRules).start();
