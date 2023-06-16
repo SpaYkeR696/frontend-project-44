115797cb834b3be328d495469853d33a49aa0fa3
SpaYkeR696 <spayker666999@gmail.com>
2023-06-16 09:37:57 +0300

    additional function

---
+++ src/utilities/prime.js
@@ -0,0 +1,11 @@
+export default (num) => {
+  if (num === 1) return false;
+  if (num % 2 === 0 && num > 2) return false;
+
+  const s = Math.sqrt(num);
+
+  for (let i = 3; i < s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
