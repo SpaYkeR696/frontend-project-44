ed286ed70e969b85347877508e969e8856f85ca8
SpaYkeR696 <spayker666999@gmail.com>
2023-06-24 10:08:54 +0300

    rewrote logic with new conditions

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,42 +1,36 @@
-import rules from '../index.js';
-import { name, getAnswerForQuestion } from '../cli.js';
+import run from '../index.js';
 import getRandomNum from '../utilities/randomNum.js';
 
-// eslint-disable-next-line no-shadow
-const greetingCalc = (rules) => {
-  const operation = ['+', '-', '*'];
+const description = 'What is the result of the expression?';
 
-  function getOperation() {
-    return operation[getRandomNum(2)];
+const operation = ['+', '-', '*'];
+
+function getOperation() {
+  return operation[getRandomNum(2)];
+}
+
+function calculation(symbol, firstNum, secondNum) {
+  switch (symbol) {
+    case '+':
+      return firstNum + secondNum;
+    case '-':
+      return firstNum - secondNum;
+    case '*':
+      return firstNum * secondNum;
+    default: return null;
   }
+}
 
-  function expression() {
-    const randomNumber1 = getRandomNum(99);
-    const randomNumber2 = getRandomNum(99);
-    const operations = getOperation();
-    const expressions = `${randomNumber1} ${operations} ${randomNumber2}`;
-    // eslint-disable-next-line no-eval
-    const rightAnswer = eval(expressions);
-    console.log(`Question: ${expressions}`);
-    const answers = getAnswerForQuestion('Your answer: ');
-    rules.check({ rightAnswer, answers }, expression);
-  }
-  return {
-    start() {
-      console.log('What is the result of the expression?');
-      expression();
-    },
-  };
+function expression() {
+  const randomNumber1 = getRandomNum(99);
+  const randomNumber2 = getRandomNum(99);
+  const operations = getOperation();
+  const question = `${randomNumber1} ${operations} ${randomNumber2}`;
+  const correctAnswer = String(calculation(operations, randomNumber1, randomNumber2));
+
+  return [question, correctAnswer];
+}
+
+export default () => {
+  run(description, expression);
 };
-
-const run = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = name();
-  console.log(`Hello, ${userName}!`);
-
-  const gameRules = rules();
-  gameRules.setUserName(userName);
-  gameRules.reset();
-  greetingCalc(gameRules).start();
-};
-export default run;
