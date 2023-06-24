a797443ae99d85f2ef53fa97e8f9a8e229150b1e
SpaYkeR696 <spayker666999@gmail.com>
2023-06-24 10:23:47 +0300

    rewrote logic with new conditions

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,35 +1,16 @@
-import rules from '../index.js';
-import { name, getAnswerForQuestion } from '../cli.js';
+import run from '../index.js';
 import getRandomNum from '../utilities/randomNum.js';
 import prime from '../utilities/prime.js';
 
-// eslint-disable-next-line no-shadow
-const greetingPrime = (rules) => {
-  function isPrime() {
-    const num = getRandomNum(7) + 2;
-    const rightAnswer = prime(num) ? 'yes' : 'no';
-    console.log(`Question: ${num}`);
-    const answers = getAnswerForQuestion('Your answer: ');
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
-    rules.check({ rightAnswer, answers }, isPrime);
-  }
+function isPrime() {
+  const num = getRandomNum(7) + 2;
+  const correctAnswer = prime(num) ? 'yes' : 'no';
 
-  return {
-    start() {
-      console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-      isPrime();
-    },
-  };
+  return [num, correctAnswer];
+}
+
+export default () => {
+  run(description, isPrime);
 };
-
-const run = () => {
-  console.log('Welcome to the Brain Games!');
-
-  const userName = name();
-  console.log(`Hello, ${userName}!`);
-  const gameRules = rules();
-  gameRules.setUserName(userName);
-  gameRules.reset();
-  greetingPrime(gameRules).start();
-};
-export default run;
