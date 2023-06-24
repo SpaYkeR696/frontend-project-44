9480bdfa398365d80009f7a5ab66b9062ef307bf
SpaYkeR696 <spayker666999@gmail.com>
2023-06-24 10:59:59 +0300

    rewrote logic with new conditions

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,47 +1,31 @@
 /* eslint-disable no-shadow */
-import rules from '../index.js';
-import { name, getAnswerForQuestion } from '../cli.js';
+import run from '../index.js';
 import getRandomNum from '../utilities/randomNum.js';
 
-const greetingProgression = (rules) => {
-  function progression(first, base, last) {
-    return [...Array(last)].map((_, i) => first + base * i);
-  }
+const description = 'What number is missing in the progression?';
 
-  function progressionByIndex(prog, index) {
-    const p = [...prog];
-    p[index] = '..';
-    return p;
-  }
+function progression(first, base, last) {
+  return [...Array(last)].map((_, i) => first + base * i);
+}
 
-  function progress() {
-    const first = getRandomNum(19);
-    const base = getRandomNum(5) + 1;
-    const last = getRandomNum(5) + 5;
-    const progressions = progression(first, base, last);
-    const rightAnswerIndex = getRandomNum(last - 1);
-    const rightAnswer = progressions[rightAnswerIndex];
-    const progShow = progressionByIndex(progressions, rightAnswerIndex).join(' ');
-    console.log(`Question: ${progShow}`);
-    const answers = getAnswerForQuestion('Your answer: ');
+function progressionByIndex(prog, index) {
+  const p = [...prog];
+  p[index] = '..';
+  return p;
+}
 
-    rules.check({ rightAnswer, answers }, progress);
-  }
-  return {
-    start() {
-      console.log('What number is missing in the progression?');
-      progress();
-    },
-  };
+function progress() {
+  const first = getRandomNum(19);
+  const base = getRandomNum(5) + 1;
+  const last = getRandomNum(5) + 5;
+  const progressions = progression(first, base, last);
+  const rightAnswerIndex = getRandomNum(last - 1);
+  const correctAnswer = String(progressions[rightAnswerIndex]);
+  const question = progressionByIndex(progressions, rightAnswerIndex).join(' ');
+
+  return [question, correctAnswer];
+}
+
+export default () => {
+  run(description, progress);
 };
-
-const run = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = name();
-  console.log(`Hello, ${userName}!`);
-  const gameRules = rules();
-  gameRules.setUserName(userName);
-  gameRules.reset();
-  greetingProgression(gameRules).start();
-};
-export default run;
